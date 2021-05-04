4bb155bc067734ec496710a218e5547ee5d5b8a6
Joy Karmakar <63857732+Joyjk@users.noreply.github.com>
2021-05-04 12:45:19 +0600

    viewModel updated

---
+++ DayTideWebApi/Models/OrderDetailsGetViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace DayTideWebApi.Models
+{
+    [NotMapped]
+    public class OrderDetailsGetViewModel
+    {
+        public string CustomerId { get; set; }
+        public int OrderId { get; set; }
+    }
+}
